5c45e00e4c9edf6325303f22094eedc651b92dda
Manuel A <manuel.alafaa@gmail.com>
2026-09-08 11:48:08 +0100

    created a script or .sh file

---
+++ extract_script.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+# This script extracts the contents of a URL to a specified directory.
+
+wget https://www.stats.govt.nz/assets/Uploads/Annual-enterprise-survey/Annual-enterprise-survey-2023-financial-year-provisional/Download-data/annual-enterprise-survey-2023-financial-year-provisional.csv
